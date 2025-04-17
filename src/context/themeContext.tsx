77c98c30c7583093df82d105c27e18169623d4de
jieun <jieunh0311@gmail.com>
2025-04-17 17:46:30 +0900

    로컬 스토리지에 저장하기

--- src/context/themeContext.tsx
+++ src/context/themeContext.tsx
@@ -1,7 +1,10 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { getTheme, ThemeName } from "../style/theme";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../style/global";
+
+const DEFAULT_THEME_NAME = "light";
+const THEME_LOCALSTORAGE_KEY = "book_store_theme";
 
 interface State {
   themeName: ThemeName;
@@ -9,8 +12,8 @@ interface State {
 }
 
 export const state = {
-  themeName: "light" as ThemeName,
-  toggleTheme:() => {},
+  themeName: DEFAULT_THEME_NAME as ThemeName,
+  toggleTheme: () => {},
 };
 
 export const ThemeContext = createContext<State>(state);
@@ -20,10 +23,18 @@ export const BookStoreThemeProvider = ({
 }: {
   children: ReactNode;
 }) => {
-  const [themeName, setThemeName] = useState<ThemeName>("light");
+  const [themeName, setThemeName] = useState<ThemeName>(DEFAULT_THEME_NAME);
   const toggleTheme = () => {
     setThemeName(themeName === "light" ? "dark" : "light");
+    localStorage.setItem(THEME_LOCALSTORAGE_KEY,themeName==="light" ? "dark" : "light")
   };
+
+  useEffect(() => {
+    const savedThemeName = localStorage.getItem
+    (THEME_LOCALSTORAGE_KEY) as ThemeName;
+    
+    setThemeName(savedThemeName || DEFAULT_THEME_NAME);
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ themeName, toggleTheme }}>
