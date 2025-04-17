dc9b3fe47eace927733e8ddbaeedbf44aa31d74c
jieun <jieunh0311@gmail.com>
2025-04-17 17:37:06 +0900

    전역 상태로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import { ThemeProvider } from 'styled-components';
-import { getTheme, ThemeName } from './style/theme'; 
-import { GlobalStyle } from './style/global';
-import Layout from './components/layout/Layout';
-import Home from './pages/Home';
-import ThemeSwitcher from './components/header/ThemeSwitcher';
-import { useState } from 'react';
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
+import ThemeSwitcher from "./components/header/ThemeSwitcher";
+import { BookStoreThemeProvider } from "./context/themeContext";
 
 function App() {
-  // 현재 UI 테마 상태 관리 (light/dark 전환)
-  const [themeName, setThemeName] = useState<ThemeName>("light");
 
   return (
     <>
-      <ThemeProvider theme={getTheme(themeName)}>
-        <GlobalStyle themeName={themeName} />
-        <ThemeSwitcher themeName={themeName} setThemeName={setThemeName} />
-        {/* <Layout children={<Home/>} /> */}
+      <BookStoreThemeProvider>
+        <ThemeSwitcher />
         <Layout>
           <Home />
         </Layout>
-      </ThemeProvider>
+      </BookStoreThemeProvider>
     </>
   );
 }
