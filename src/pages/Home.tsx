a6b82206ac9388dfcc046107713667df2a82349c
jieun <jieunh0311@gmail.com>
2025-04-17 16:48:02 +0900

    home body 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,8 @@
-import Header from "../components/common/Header";
-import { formatNumber } from "../utils/format";
 
-const COUNT = 10000;
 function Home(){
     return (
     <>
-        <Header/>
         <div>home body</div>
-        <div>count: {formatNumber(COUNT)}</div>
     </>
     );
 }
