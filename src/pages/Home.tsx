5bb510bf02de52326e3eea5bf231b05fa4181f94
jieun <jieunh0311@gmail.com>
2025-04-18 02:37:34 +0900

    Button 컴포넌트 작성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Title from "../components/common/Title";
+import Button from "../components/common/Button";
 
 function Home(){
     return (
     <>
     <Title size="medium" color="background">제목 테스트</Title>
+    <Button size="large" scheme="normal">버튼 테스트</Button>
         <div>home body</div>
     </>
     );
