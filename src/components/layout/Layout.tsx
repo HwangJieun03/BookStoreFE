9a3000d21b201bcc32e4f2fe606f80c868388fa1
jieun <jieunh0311@gmail.com>
2025-04-18 16:13:43 +0900

    react-icons 설치

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,26 @@
+import styled from "styled-components";
 import Footer from "../common/Footer";
 import Header from "../common/Header";
 
 interface LayoutProps {
-    children : React.ReactNode;
+  children: React.ReactNode;
 }
 
-function Layout({children}:LayoutProps){
-    return(
-        <>
-            <Header/>
-            <main>{children}</main>
-            <Footer/>
-        </>
-    )
+function Layout({ children }: LayoutProps) {
+  return (
+    <LayoutStyle>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </LayoutStyle>
+  );
 }
+
+const LayoutStyle = styled.main`
+  width: 100%;
+  margin: 0 auto; // 가운데 정렬
+  max-width: ${({ theme }) => theme.layout.width.large};
+  padding : 20px 0;
+`;
 
 export default Layout;
