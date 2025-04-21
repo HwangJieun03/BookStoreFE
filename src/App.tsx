d7d97fcb4c6d93e93b763024da3247eca0f774e7
jieun <jieunh0311@gmail.com>
2025-04-21 10:18:52 +0900

    라우터 작성

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import ThemeSwitcher from "./components/header/ThemeSwitcher";
 import { BookStoreThemeProvider } from "./context/themeContext";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <Home />,
+  },
+  {
+    path : "/books",
+    element : <div>도서 목록</div>
+  }
+])
 function App() {
 
   return (
@@ -10,7 +21,7 @@ function App() {
       <BookStoreThemeProvider>
         <ThemeSwitcher />
         <Layout>
-          <Home />
+          <RouterProvider router = {router}/>
         </Layout>
       </BookStoreThemeProvider>
     </>
