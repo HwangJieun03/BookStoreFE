f7f83768621ada425ed9b0b44e9764cd482548cf
jieun <jieunh0311@gmail.com>
2025-04-22 17:34:15 +0900

    필터 버튼 클릭 시 URL 쿼리스트링 및 active 상태 반영 기능 구현

--- src/components/books/BooksFilter.tsx
+++ src/components/books/BooksFilter.tsx
@@ -1,35 +1,69 @@
-import { styled } from 'styled-components';
-import { useCategory } from '../../hooks/useCategory';
-import Button from '../common/Button';
+import { styled } from "styled-components";
+import { useCategory } from "../../hooks/useCategory";
+import Button from "../common/Button";
+import { useSearchParams } from "react-router-dom";
 
 function BooksFilter() {
+  const { category } = useCategory();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const {category} = useCategory();
-  
+  const handleCategory = (id: number | null) => {
+    const newSearchParams = new URLSearchParams(searchParams);
+
+    if (id === null) {
+      newSearchParams.delete("category_id");
+    } else {
+      newSearchParams.set("category_id", id.toString());
+    }
+
+    setSearchParams(newSearchParams);
+  };
+
+  const handleNews = () => {
+    const newSearchParams = new URLSearchParams(searchParams);
+
+    if (newSearchParams.get("news")) {
+      newSearchParams.delete("news");
+    } else {
+      newSearchParams.set("news", "true");
+    }
+
+    setSearchParams(newSearchParams);
+  };
+
   return (
     <BooksFilterStyle>
       <div className="category">
-        {
-          category.map((item) => (
-            <Button size="medium" scheme="normal" key={item.category_id}>
-              {item.category_name}
-            </Button>
-          ))
-        }
+        {category.map((item) => (
+          <Button
+            size="medium"
+            scheme={item.isActive ? "primary" : "normal"}
+            key={item.category_id}
+            onClick={() => handleCategory(item.category_id)}
+          >
+            {item.category_name}
+          </Button>
+        ))}
       </div>
       <div className="new">
-        <Button size="medium" scheme='normal'>신간</Button>
+        <Button
+          size="medium"
+          scheme={searchParams.get("news") ? "primary" : "normal"}
+          onClick={() => handleNews()}
+        >
+          신간
+        </Button>
       </div>
     </BooksFilterStyle>
   );
-};
+}
 
 const BooksFilterStyle = styled.div`
   display: flex;
-  gap : 24px;
-  .category{
+  gap: 24px;
+  .category {
     display: flex;
-    gap : 8px;
+    gap: 8px;
   }
 `;
 
