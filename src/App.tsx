547790af8c881aef5b9cfdc32e1bb3ed7df5276a
jieun <jieunh0311@gmail.com>
2025-04-22 13:09:41 +0900

    로그인 기초 작성

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Error from "./components/common/Error";
 import Signup from "./pages/Signup";
 import ResetPassword from "./pages/ResetPassword";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
   {
     path : "/reset",
     element : <Layout><ResetPassword/></Layout>
+  },
+  {
+    path : "/login",
+    element : <Layout><Login/></Layout>
   }
 ])
 
