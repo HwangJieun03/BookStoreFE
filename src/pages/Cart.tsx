c8ac819122b362e5802ffff00aab3d33e81c240a
jieun <jieunh0311@gmail.com>
2025-04-23 18:20:37 +0900

    비어있는 장바구니 구현

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,9 +3,13 @@ import Title from "../components/common/Title";
 import CartItem from "../components/cart/CartItem";
 import { useCart } from "../hooks/useCart";
 import { useState } from "react";
+import Empty from "../components/common/Empty";
+import { FaShoppingCart } from "react-icons/fa";
+
+const ShoppingCartIcon = FaShoppingCart as React.ElementType;
 
 function Cart() {
-  const { carts , deleteCartItem} = useCart();
+  const { carts, deleteCartItem, isEmpty } = useCart();
   const [checkedItems, setCheckedItems] = useState<number[]>([]);
 
   const handleCheckItem = (id: number) => {
@@ -18,27 +22,38 @@ function Cart() {
     }
   };
 
-  const handleDeleteItem = (id:number) => {
-    // 삭제 행위 
+  const handleDeleteItem = (id: number) => {
+    // 삭제 행위
     deleteCartItem(id);
-  }
+  };
 
   return (
     <>
       <Title size="large">장바구니</Title>
       <CartStyle>
-        <div className="content">
-          {carts.map((item) => (
-            <CartItem
-              key={item.id}
-              cart={item}
-              checkedItems={checkedItems}
-              onCheck={handleCheckItem}
-              onDelete={handleDeleteItem}
-            />
-          ))}
-        </div>
-        <div className="summary"></div>
+        {!isEmpty && (
+          <>
+            <div className="content">
+              {carts.map((item) => (
+                <CartItem
+                  key={item.id}
+                  cart={item}
+                  checkedItems={checkedItems}
+                  onCheck={handleCheckItem}
+                  onDelete={handleDeleteItem}
+                />
+              ))}
+            </div>
+            <div className="summary"></div>
+          </>
+        )}
+        {isEmpty && (
+          <Empty
+            title="장바구니가 비어있습니다."
+            icon={<ShoppingCartIcon />}
+            description={<>구매하실 상품을 장바구니에 담아주세요.</>}
+          />
+        )}
       </CartStyle>
     </>
   );
