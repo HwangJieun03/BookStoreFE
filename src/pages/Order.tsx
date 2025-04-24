62fc53aecc292fb210a567adee8eebe95b29aec0
jieun <jieunh0311@gmail.com>
2025-04-24 14:06:58 +0900

    주문 데이터 서버에 전달

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Title from "../components/common/Title";
 import { CartStyle } from "./Cart";
 import CartSummary from "../components/cart/CartSummary";
@@ -6,19 +6,25 @@ import Button from "../components/common/Button";
 import InputText from "../components/common/InputText";
 import { useForm } from "react-hook-form";
 import { Delivery, OrderSheet } from "../models/order.model";
+import FindAddressButton from "../components/order/FindAddressButton";
+import { useAlert } from "../hooks/useAlert";
+import { order } from "../api/order.api";
 
 interface DeliveryForm extends Delivery {
   addressDetail: string;
 }
 
 function Order() {
+  const {showAlert, showConfirm} = useAlert();
   const location = useLocation();
+  const navigate = useNavigate();
   const orderDataFromCart = location.state;
   const { totalQuantity, totalPrice, firstBookTitle } = orderDataFromCart;
 
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
   } = useForm<DeliveryForm>();
 
@@ -31,8 +37,12 @@ function Order() {
       },
     };
 
-    // 서버로 넘겨준다.
-    console.log(orderData);
+    showConfirm("주문을 진행하시겠습니까?", ()=>(
+      order(orderData).then(()=>{
+      showAlert("주문이 처리되었습니다.");
+      navigate("/orderlist");
+    })
+    ))
   };
 
   return (
@@ -53,9 +63,11 @@ function Order() {
                     {...register("address", { required: true })}
                   />
                 </div>
-                <Button size="small" scheme="normal">
-                  주소 찾기
-                </Button>
+                <FindAddressButton
+                  onCompleted={(address) => {
+                    setValue('address', address);
+                  }}
+                />
               </fieldset>
               {errors.address && (
                 <p className="error-text">주소를 입력해 주세요</p>
