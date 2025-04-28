66d5af8e71c6efb7173d6e6e7c60b1649adad87a
jieun <jieunh0311@gmail.com>
2025-04-28 17:40:13 +0900

    리뷰 목록 추가

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom";
 import EllipsisBox from "../components/common/EllipsisBox";
 import LikeButton from "../components/book/LikeButton";
 import AddToCart from "../components/book/AddToCart";
+import BookReview from "@/components/book/BookReview";
 
 const bookInfoList = [
   {
@@ -88,6 +89,9 @@ function BookDeatil() {
         
         <Title size="medium">목차</Title>
         <p className="index">{book.contents}</p>
+
+        <Title size="medium">리뷰</Title>
+        <BookReview reviews={reviews}/>
       </div>
     </BookDeatilStyle>
   );
