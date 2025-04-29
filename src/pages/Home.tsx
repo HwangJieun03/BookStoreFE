ff51a0280d728bfddd1c54b3e25e1a77c405d4c9
jieun <jieunh0311@gmail.com>
2025-04-29 17:54:46 +0900

    메인 페이지 리뷰 섹션

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
+import MainReview from "@/components/main/MainReview";
+import { useMain } from "@/hooks/useMain";
 import { styled } from "styled-components";
 
 function Home() {
+  const {reviews} = useMain();
+
   return (
     <HomeStyle>
-      <h1>Home</h1>
+      {/* 배너 */}
+
+      {/* 베스트셀러 */}
+
+      {/* 신간 */}
+
+      {/* 리뷰 */}
+      <MainReview reviews={reviews}/>
     </HomeStyle>
   );
 }
@@ -11,6 +22,5 @@ function Home() {
 const HomeStyle = styled.div`
   color: ${({ theme }) => theme.color.primary};
 `;
-
 
 export default Home;
